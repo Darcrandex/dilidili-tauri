3325f0fdbc75ed98e88a465d2467ffe80d01d2d7
darcrand <darcrandex@gmail.com>
2025-02-20 15:01:16 +0800

    clear

--- src/core/save-info.ts
+++
@@ -1,7 +0,0 @@
-// 保存 bv 视频的信息，避免重复请求
-
-import { writeTextFile } from '@tauri-apps/api/fs'
-
-export async function saveInfo(filePath: string, data: Record<string, any>) {
-  await writeTextFile(filePath, JSON.stringify(data))
-}
