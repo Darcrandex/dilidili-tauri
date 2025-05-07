b2de601c65573d4aa7f9724e6b90bb867175502b
darcrand <darcrandex@gmail.com>
2025-05-07 14:35:42 +0800

    🎈 perf(build): 更新发布脚本

--- release-version.sh
+++ release-version.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# 设置默认的升级类型为 patch
+UPGRADE_TYPE="patch"
+
+# 若提供了参数，则使用传入的参数
+if [ $# -gt 0 ]; then
+    UPGRADE_TYPE=$1
+fi
+
+# 检查参数是否合法
+if [ "$UPGRADE_TYPE" != "major" ] && [ "$UPGRADE_TYPE" != "minor" ] && [ "$UPGRADE_TYPE" != "patch" ]; then
+    echo "无效的升级类型参数。请使用 major、minor 或 patch。"
+    exit 1
+fi
+
 # 读取 package.json 中的版本号
 VERSION=$(jq -r '.version' package.json)
 
@@ -8,9 +22,24 @@ MAJOR=$(echo $VERSION | cut -d. -f1)
 MINOR=$(echo $VERSION | cut -d. -f2)
 PATCH=$(echo $VERSION | cut -d. -f3)
 
-# 提升小版本号
-NEW_PATCH=$((PATCH + 1))
-NEW_VERSION="$MAJOR.$MINOR.$NEW_PATCH"
+# 根据升级类型更新版本号
+case $UPGRADE_TYPE in
+    major)
+        NEW_MAJOR=$((MAJOR + 1))
+        NEW_MINOR=0
+        NEW_PATCH=0
+        NEW_VERSION="$NEW_MAJOR.$NEW_MINOR.$NEW_PATCH"
+        ;;
+    minor)
+        NEW_MINOR=$((MINOR + 1))
+        NEW_PATCH=0
+        NEW_VERSION="$MAJOR.$NEW_MINOR.$NEW_PATCH"
+        ;;
+    patch)
+        NEW_PATCH=$((PATCH + 1))
+        NEW_VERSION="$MAJOR.$MINOR.$NEW_PATCH"
+        ;;
+esac
 
 # 更新 package.json 中的版本号
 jq --arg new_version "$NEW_VERSION" '.version = $new_version' package.json > package.json.tmp
@@ -31,4 +60,4 @@ git tag -a "app-v$NEW_VERSION" -m "v$NEW_VERSION"
 git push origin main
 git push --tags
 
-echo "成功发布版本 $NEW_VERSION"    
+echo "成功发布版本 $NEW_VERSION"
