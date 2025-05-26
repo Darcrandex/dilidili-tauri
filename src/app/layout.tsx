a1a5d3cc1cd8c3386f325d8d8e97e7a5da67c2c1
darcrand <darcrandex@gmail.com>
2025-05-26 09:00:39 +0700

    🐞 fix(首页): 添加检查版本功能

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,33 @@
  */
 
 import AsideMenus from '@/components/AsideMenus'
-import { Suspense } from 'react'
+import { notification } from '@/components/GlobalAntdMessage'
+import { useLatestVersion } from '@/hooks/useLatestVersion'
+import { Suspense, useEffect } from 'react'
 import { Outlet } from 'react-router'
 
 export default function AppLayout() {
+  const { latestVersion, hasUpdate } = useLatestVersion()
+
+  useEffect(() => {
+    if (hasUpdate) {
+      notification.info({
+        key: 'app-update',
+        duration: 0,
+        message: '发现新版本',
+        description: (
+          <a
+            href={`https://github.com/${import.meta.env.VITE_APP_GIT_OWNER}/${import.meta.env.VITE_APP_GIT_REPO}/releases/latest`}
+            target='_blank'
+            rel='noreferrer'
+          >
+            点击查看最新版本：{latestVersion}
+          </a>
+        ),
+      })
+    }
+  }, [hasUpdate, latestVersion])
+
   return (
     <>
       <section className='flex h-screen flex-1'>
